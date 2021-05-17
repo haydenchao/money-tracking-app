23fc008f0c3ba2f35b4417d5636af022bd2944fc
hayden chao <chaokenghin@gmail.com>
2021-05-17 21:31:20 +0800

    feat: New groupby function for transactions
    Added a new function using dataframe groupby function. It can show a
    summary of the amount for each different transactions.
    
    Further desired ideas:
    - Add-in date columns
    - filter data with the date as well, can use to produce
    a monthly summary

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import sqlite3 as sql3
+import pandas as pd
 
 
 MENU_PROMPT = """ -- Money Tracker App --
@@ -18,17 +19,32 @@ INSERT_QUERY = "INSERT INTO transactions (description, amount) VALUES (?,?)"
 
 def add_item(cursor):
     print("*" * 20)
-    desc = input("Enter your transaction name: ")
+    desc = input("Enter your transaction name: ").capitalize()
     amount = float(input("Enter the transaction amount: "))
-    print("*" * 20, '\n')
+    print("*" * 20, '')
+    print("{} number of row(s) updated\n".format(cursor.rowcount))
     items = (desc, amount)
     cursor.execute(INSERT_QUERY, items)
+
+def trans_groupby_name(cursor):
+    items = {}
+    for id, desc, amount in cursor.execute("SELECT * FROM transactions"):
+        items[id] = [desc, amount]
+
+    df = pd.DataFrame.from_dict(items,orient='index')
+    df.columns = ["description","amount"]
+    df = df.groupby(['description']).sum()
+    print("-" * 20)
+    print(df)
+    print("-" * 20, '\n')
+
+    while input("Press any key to continue:\n"):
+        break
 
 
 def data_export_txt(text_file,cursor):
     with open(text_file, "w+") as file:
-        if file.readline == []:
-            file.write("ID, Description, Amount\n")
+        file.write("ID, Description, Amount\n")
 
         for i in cursor.execute("SELECT * FROM transactions").fetchall():
             file.write("{}, {}, {} \n".format(i[0], i[1], i[2]))
@@ -38,13 +54,8 @@ def main():
     conn = sql3.connect("data.db")
     cursor = conn.cursor()
 
-
     cursor.execute("CREATE TABLE IF NOT EXISTS transactions(id INTEGER PRIMARY KEY, Description TEXT NOT NULL, Amount FLOAT NOT NULL)")
-    #cursor.execute("INSERT INTO transactions (Description, Amount) VALUES('Eating out', 12)")
     cursor.execute("PRAGMA table_info(tablename)")
-
-
-
 
 
     while (user_input := input(MENU_PROMPT)) != "6":
@@ -53,7 +64,7 @@ def main():
             add_item(cursor)
 
         elif user_input == "2":
-            pass
+            trans_groupby_name(cursor)
 
         elif user_input == "3":
             pass
@@ -66,8 +77,8 @@ def main():
         else:
             print("Invalid input. ")
 
-
-    conn.commit()
+    cursor.connection.commit()
+    #conn.commit(), cursor.connection is perferable
     conn.close()
 
 if __name__ == "__main__":
